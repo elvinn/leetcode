48abe1b13c0b0870318c1290f799aacea88bd95e
Elvin Peng <elvinpeng@tencent.com>
2020-07-04 11:24:26 +0800

    feat: 287 find-the-duplicate-number O(n) solution

--- src/287-find-the-duplicate-number.cpp
+++ src/287-find-the-duplicate-number.cpp
@@ -18,7 +18,7 @@ public:
    * Time Complexity: O(n * log(n))
    * Space Complexity: O(1)
    */
-  int findDuplicate(vector<int> &nums) {
+  int findDuplicate1(vector<int> &nums) {
     int low = 1;
     int high = nums.size() - 1;
     while (low < high) {
@@ -39,6 +39,27 @@ public:
 
     return low;
   }
+  /**
+   * fast-slow pointer
+   * Time Complexity: O(n)
+   * Space Complexity: O(1)
+   */
+  int findDuplicate2(vector<int> &nums) {
+    int slow = 0;
+    int fast = 0;
+    do {
+      slow = nums[slow];
+      fast = nums[nums[fast]];
+    } while (slow != fast);
+
+    slow = 0;
+    while (slow != fast) {
+      slow = nums[slow];
+      fast = nums[fast];
+    }
+
+    return slow;
+  }
 };
 
 int main() {
@@ -48,9 +69,13 @@ int main() {
   vector<int> input2{3, 1, 3, 4, 2};
   vector<int> input3{1, 1};
 
-  assert(mySolution.findDuplicate(input1) == 2);
-  assert(mySolution.findDuplicate(input2) == 3);
-  assert(mySolution.findDuplicate(input3) == 1);
+  assert(mySolution.findDuplicate1(input1) == 2);
+  assert(mySolution.findDuplicate1(input2) == 3);
+  assert(mySolution.findDuplicate1(input3) == 1);
+
+  assert(mySolution.findDuplicate2(input1) == 2);
+  assert(mySolution.findDuplicate2(input2) == 3);
+  assert(mySolution.findDuplicate2(input3) == 1);
 
   return 0;
 }
